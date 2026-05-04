b1007896bcc4e8c25679a11222e4efe0bf5aa1b6
Nguyen <huunguyen.personal@gmail.com>
2026-05-04 22:12:37 +0700

    UI: added PrimaryButton, add styling to LoginPanel and RegisterPanel

--- client-app/src/gui/auth/LoginPanel.java
+++ client-app/src/gui/auth/LoginPanel.java
@@ -1,36 +1,78 @@
 package gui.auth;
 
+import gui.components.AuthHeader;
 import gui.components.FormField;
+import gui.components.PrimaryButton;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class LoginPanel extends JPanel {
-    private final FormField usernameField;
-    private final FormField passwordField;
-    private final JButton loginButton;
-
     public LoginPanel() {
         setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
-        setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+        setBorder(BorderFactory.createEmptyBorder(40, 40, 40, 40));
+        setBackground(Color.decode("#313338"));
 
-        usernameField = new FormField("Username", false);
-        passwordField = new FormField("Password", true);
-        loginButton = new JButton("Login");
+        // Header
+        AuthHeader header = new AuthHeader("Welcome back!", "We're excited to see you again");
 
-        loginButton.setAlignmentX(Component.CENTER_ALIGNMENT); // Căn giữa
+        // Fields
+        FormField usernameField = new FormField("Username", "Enter your username or email", false);
+        FormField passwordField = new FormField("Password", "Enter your password", true);
 
-        add(usernameField);
-        add(Box.createVerticalStrut(15));
-        add(passwordField);
-        add(Box.createVerticalStrut(20));
-        add(loginButton);
+        // Forgot Password Link
+        JLabel forgotPass = new JLabel("Forgot your password?");
+        forgotPass.setForeground(Color.decode("#00A8FC"));
+        forgotPass.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        forgotPass.setAlignmentX(Component.LEFT_ALIGNMENT);
+        JPanel linkPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        linkPanel.setBackground(Color.decode("#313338"));
+        linkPanel.add(forgotPass);
 
-        // MOCK CLICK EVENT (WILL BE REPLACED WITH API CALL)
-        loginButton.addActionListener(e -> {
+        // Login Button
+        PrimaryButton loginButton = new PrimaryButton("Log In", e -> {
             String user = usernameField.getText();
             String pass = passwordField.getText();
-            System.out.println("Attempting to login with: " + user + " / " + pass);
-            // TODO: Replace with api call
+            System.out.println("Attempting login with: " + user + " / " + pass);
+            // TODO: Call ApiClient POST /api/auth/login
+        });
+
+        // Register Link
+        JLabel registerLink = new JLabel("<html><font color='#B5BAC1'>Need an account?</font> <font color='#00A8FC'>Register</font></html>");
+        registerLink.setCursor(new Cursor(Cursor.HAND_CURSOR));
+        JPanel footerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT, 0, 0));
+        footerPanel.setBackground(Color.decode("#313338"));
+        footerPanel.add(registerLink);
+
+        // ---ASSEMBLE---
+        add(header);
+        add(Box.createVerticalStrut(30));
+
+        usernameField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(usernameField);
+        add(Box.createVerticalStrut(15));
+
+        passwordField.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(passwordField);
+        add(Box.createVerticalStrut(5));
+
+        linkPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(linkPanel);
+        add(Box.createVerticalStrut(20));
+
+        add(loginButton);
+        add(Box.createVerticalStrut(10));
+
+        footerPanel.setAlignmentX(Component.CENTER_ALIGNMENT);
+        add(footerPanel);
+
+        registerLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                System.out.println("Switching to Register Tab...");
+                // We will handle tab switching later!
+            }
         });
     }
 }
