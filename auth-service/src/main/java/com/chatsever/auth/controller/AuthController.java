b956634484c6be6eb6be0eda6be9649a4edbfeb6
pp20082006 <c3lttrong.0a1.ntphat@gmail.com>
2026-05-05 17:22:20 +0700

    auth-service

---
+++ auth-service/src/main/java/com/chatsever/auth/controller/AuthController.java
@@ -0,0 +1,26 @@
+package com.chatsever.auth.controller;
+
+import com.chatsever.auth.dto.AuthRequest;
+import com.chatsever.auth.dto.AuthResponse;
+import com.chatsever.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthResponse> login(@RequestBody AuthRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
---
+++ auth-service/src/main/java/com/chatsever/auth/dto/AuthRequest.java
@@ -0,0 +1,9 @@
+package com.chatsever.auth.dto;
+
+import lombok.*;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+public class AuthRequest {
+    private String username;
+    private String password;
+}
