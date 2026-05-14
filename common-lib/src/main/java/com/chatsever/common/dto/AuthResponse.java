ccc3ba5a1d609d835e0c0987152119c393047806
pp20082006 <c3lttrong.0a1.ntphat@gmail.com>
2026-05-15 00:01:03 +0700

    new code

--- common-lib/src/main/java/com/chatsever/common/dto/AuthResponse.java
+++ common-lib/src/main/java/com/chatsever/common/dto/AuthResponse.java
@@ -2,19 +2,13 @@ package com.chatsever.common.dto;
 
 import lombok.*;
 
-@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
     private String token;
     private String username;
 
-    public AuthResponse() {}
-    public AuthResponse(String token, String username) {
-        this.token = token;
-        this.username = username;
-    }
-
-    public String getToken() { return token; }
-    public void setToken(String token) { this.token = token; }
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
 }
